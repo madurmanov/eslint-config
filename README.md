cda4aea14ddc8ad91d36340fc831249ca7f64cb6
Mikhail Durmanov <madurmanov@gmail.com>
2018-01-19 14:23:38 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# eslint-config
+
+## License
+**eslint-config** is released under the MIT License. See the bundled `LICENSE.md` for details.
---
+++ index.js
@@ -0,0 +1,87 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [
+    'react'
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
+  },
+  env: {
+    browser: true,
+    jest: true
+  },
+  globals: {
+    __PROJECT__: true,
+    __BROWSER__: true,
+    __DEV__: true,
+    __LOC__: true,
+    document: true,
+    window: true,
+    jest: true
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {}
+    }
+  },
+  rules: {
+    'global-require': 0,
+    'arrow-body-style': 0,
+    'arrow-parens': 0,
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore'
+      }
+    ],
+    'no-console': 0,
+    semi: [
+      2,
+      'never'
+    ],
+
+    'import/extensions': 0,
+    'import/no-unresolved': 1,
+    'import/no-extraneous-dependencies': [
+      1,
+      {
+        'devDependencies': [
+          '**/*.test.js',
+          '**/*.spec.js'
+        ]
+      }
+    ],
+    'import/prefer-default-export': 0,
+
+    'jsx-a11y/href-no-hash': 0,
+    'jsx-a11y/img-has-alt': 0,
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'react/no-array-index-key': 0,
+    'react/prefer-stateless-function': 0,
+    'react/prop-types': 0,
+
+    'angular/controller-name': 0,
+    'angular/definedundefined': 0,
+    'angular/document-service': 0,
+    'angular/interval-service': 0,
+    'angular/json-functions': 0,
+    'angular/log': 0,
+    'angular/timeout-service': 0,
+    'angular/typecheck-array': 0,
+    'angular/typecheck-function': 0,
+    'angular/typecheck-object': 0,
+    'angular/typecheck-string': 0,
+    'angular/window-service': 0
+  }
+};
