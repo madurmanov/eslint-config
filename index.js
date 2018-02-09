f5b49438025d7371e14c6a9283e113c21c22f692
Mikhail Durmanov <madurmanov@gmail.com>
2018-02-09 19:29:18 +0300

    Update configuration

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb',
   globals: {
-    __BROWSER__: true,
-    __DEV__: true,
+    window: true,
+    document: true,
+    alert: true,
   },
   rules: {
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    'max-len': [
+      2,
+      {
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'global-require': 0,
+    'function-paren-newline': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
     semi: [2, 'never'],
   },
 }
